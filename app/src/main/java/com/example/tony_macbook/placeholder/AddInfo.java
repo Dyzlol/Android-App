466e361968ed3abacefda464931d708d5dcde8f0
Tony <harry.a.young@gmail.com>
2016-04-18 20:15:17 -0400

    Added Upload Image placeholder Added submit button listener

--- app/src/main/java/com/example/tony_macbook/placeholder/AddInfo.java
+++ app/src/main/java/com/example/tony_macbook/placeholder/AddInfo.java
@@ -1,18 +1,60 @@
 package com.example.tony_macbook.placeholder;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Tony-MacBook on 4/18/16.
  */
-public class AddInfo extends AppCompatActivity {
+public class AddInfo extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addinfo);
+
+        Button uploadButton = (Button) findViewById(R.id.upload_photo);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder saveDialog = new AlertDialog.Builder(AddInfo.this);
+                saveDialog.setTitle("Upload Photo");
+                saveDialog.setMessage("This is currently a placeholder");
+                saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                saveDialog.show();
+            }
+        });
+
+        Button submitButton = (Button) findViewById(R.id.submit_info);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick (View v) {
+                    Intent intent = new Intent(AddInfo.this, AddInfo.class);
+                    startActivity(intent);
+                }
+        });
+
+
     }
+
+
+
+
+
 
 
 }
